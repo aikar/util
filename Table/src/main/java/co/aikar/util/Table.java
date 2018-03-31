32361e88f6a10f1899d2cac9939ca29884899923
Aikar <aikar@aikar.co>
2018-03-30 21:38:45 -0400

    Finish table class

--- Table/src/main/java/co/aikar/util/Table.java
+++ Table/src/main/java/co/aikar/util/Table.java
@@ -3,101 +3,278 @@ package co.aikar.util;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Spliterators;
 import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class Table <R, C, V> {
 
-    private final Map<R, Map<C, V>> backingRowMap;
-    private final Supplier<Map<C, V>> columnMapSupplier;
+    private final Map<R, Map<C, V>> rowMap;
+    private final Function<R, Map<C, V>> colMapSupplier;
 
     public Table() {
-        this(new HashMap<>(), HashMap::new);
+        this(new HashMap<>(), r -> new HashMap<>());
     }
 
-    public Table(Map<R, Map<C, V>> backingRowMap, Supplier<Map<C, V>> columnMapSupplier) {
-        this.backingRowMap = backingRowMap;
-        this.columnMapSupplier = columnMapSupplier;
+    public Table(Map<R, Map<C, V>> backingRowMap, Function<R, Map<C, V>> columnMapSupplier) {
+        this.rowMap = backingRowMap;
+        this.colMapSupplier = columnMapSupplier;
+    }
+
+
+    public V get(R row, C col) {
+        return getIfExists(row, col);
+    }
+
+    public V getOrDefault(R row, C col, V def) {
+        Map<C, V> colMap = getColMapIfExists(row);
+        if (colMap == null) {
+            return def;
+        }
+
+        V v = colMap.get(col);
+        if (v != null || colMap.containsKey(col)) {
+            return v;
+        }
+        return def;
+    }
+
+    public boolean containsKey(R row, C col) {
+        Map<C, V> colMap = getColMapIfExists(row);
+        if (colMap == null) {
+            return false;
+        }
+        return colMap.containsKey(col);
     }
 
     @Nullable
-    public Map<C, V> put(R r, C col, V val) {
+    public V put(R row, C col, V val) {
+        return getColMapForWrite(row).put(col, val);
+    }
+
+    public void forEach(TableConsumer<R, C, V> consumer) {
+        for (Iterator<Entry<R, C, V>> it = this.iterator(); it.hasNext(); ) {
+            Entry<R, C, V> entry = it.next();
+            consumer.accept(entry.getRow(), entry.getCol(), entry.getValue());
+        }
+    }
+
+    public void forEach(TablePredicate<R, C, V> predicate) {
+        for (Iterator<Entry<R, C, V>> it = this.iterator(); it.hasNext(); ) {
+            Entry<R, C, V> entry = it.next();
+            if (!predicate.test(entry.getRow(), entry.getCol(), entry.getValue())) {
+                return;
+            }
+        }
+    }
+
+    public void removeIf(TablePredicate<R, C, V> predicate) {
+        for (Iterator<Entry<R, C, V>> it = this.iterator(); it.hasNext(); ) {
+            Entry<R, C, V> entry = it.next();
+            if (predicate.test(entry.getRow(), entry.getCol(), entry.getValue())) {
+                it.remove();
+            }
+        }
+    }
+
+    public Stream<Entry<R, C, V>> stream() {
+        return stream(false);
+    }
+
+    public Stream<Entry<R, C, V>> stream(boolean parallel) {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator(), 0), parallel);
+    }
+
+    public Iterator<Entry<R, C, V>> iterator() {
+        return new Iterator<Entry<R, C, V>>() {
+            Iterator<Map.Entry<R, Map<C, V>>> rowIter = rowMap.entrySet().iterator();
+            Iterator<Map.Entry<C, V>> colIter = null;
+
+            private Map.Entry<R, Map<C, V>> rowEntry;
+            private Map.Entry<C, V> colEntry;
+
+            private Entry<R, C, V> next = getNext();
+
+            private Entry<R, C, V> getNext() {
+                if (colIter == null || !colIter.hasNext()) {
+                    if (!rowIter.hasNext()) {
+                        return null;
+                    }
+                    rowEntry = rowIter.next();
+                    colIter = rowEntry.getValue().entrySet().iterator();
+                }
+
+                if (!colIter.hasNext()) {
+                    return null;
+                }
+
+                colEntry = colIter.next();
+
+                return new Node(rowEntry, colEntry);
+            }
+
+            @Override
+            public boolean hasNext() {
+                return this.next != null;
+            }
+
+            @Override
+            public Entry<R, C, V> next() {
+                Entry<R, C, V> entry = this.next;
+                this.next = getNext();
+                return entry;
+            }
+
+            @Override
+            public void remove() {
+                this.colIter.remove();
+            }
+        };
+    }
+
+    public void replaceAll(TableFunction<R, C, V, V> function) {
+        for (Iterator<Entry<R, C, V>> it = this.iterator(); it.hasNext(); ) {
+            Entry<R, C, V> entry = it.next();
+            entry.setValue(function.compose(entry.getRow(), entry.getCol(), entry.getValue()));
+        }
+    }
+
+    public V remove(R row, C col) {
+        Map<C, V> rowMap = this.rowMap.get(row);
+        if (rowMap == null) {
+            return null;
+        }
+        return rowMap.remove(col);
+    }
+
+    @Nullable
+    public V replace(R row, C col, V val) {
+        Map<C, V> rowMap = getColMapIfExists(row);
+        if (rowMap == null) {
+            return null;
+        }
+        if (rowMap.get(col) != null || rowMap.containsKey(col)) {
+            return rowMap.put(col, val);
+        }
         return null;
     }
 
 
-    public interface ForEachStoppableConsumer <R, C, V> {
-        boolean accept(R row, C col, V val);
+    @Nullable
+    public boolean replace(R row, C col, V old, V val) {
+        Map<C, V> rowMap = getColMapIfExists(row);
+        if (rowMap == null) {
+            return false;
+        }
+        if (Objects.equals(rowMap.get(col), old)) {
+            rowMap.put(col, val);
+            return true;
+        }
+        return false;
     }
-    public interface ForEachConsumer <R, C, V> {
+
+    public V computeIfAbsent(R row, C col, BiFunction<R, C, V> function) {
+        return getColMapForWrite(row).computeIfAbsent(col, c -> function.apply(row, col));
+    }
+
+    public V computeIfPresent(R row, C col, TableFunction<R, C, V, V> function) {
+        Map<C, V> colMap = getColMapForWrite(row);
+        V v = colMap.computeIfPresent(col, (c, old) -> function.compose(row, col, old));
+        removeIfEmpty(row, colMap);
+        return v;
+    }
+
+    public V compute(R row, C col, TableFunction<R, C, V, V> function) {
+        Map<C, V> colMap = getColMapForWrite(row);
+        V v = colMap.compute(col, (c, old) -> function.compose(row, col, old));
+        removeIfEmpty(row, colMap);
+        return v;
+    }
+
+    public V merge(R row, C col, V val, TableFunction<R, C, V, V> function) {
+        Map<C, V> colMap = getColMapForWrite(row);
+        V v = colMap.merge(col, val, (c, old) -> function.compose(row, col, old));
+        removeIfEmpty(row, colMap);
+        return v;
+    }
+
+    // Other stuff
+
+    public interface TablePredicate<R, C, V> {
+        boolean test(R row, C col, V val);
+    }
+    public interface TableFunction<R, C, V, RETURN> {
+        RETURN compose(R row, C col, V val);
+    }
+    public interface TableConsumer<R, C, V> {
         void accept(R row, C col, V val);
     }
 
-    public void forEach(ForEachConsumer<R, C, V> consumer) {
-        for (Map.Entry<R, Map<C, V>> rowEntry : backingRowMap.entrySet()) {
-            R row = rowEntry.getKey();
-            for (Map.Entry<C, V> colEntry : rowEntry.getValue().entrySet()) {
-                C col = colEntry.getKey();
-                consumer.accept(row, col, colEntry.getValue());
-            }
+    private V getIfExists(R row, C col) {
+        Map<C, V> colMap = getColMapIfExists(row);
+        if (colMap == null) {
+            return null;
+        }
+
+        return colMap.get(col);
+    }
+
+    private Map<C, V> getColMapIfExists(R row) {
+        Map<C, V> rowMap = this.rowMap.get(row);
+        if (rowMap == null) {
+            return null;
+        }
+        return rowMap;
+    }
+    private Map<C, V> getColMapForWrite(R row) {
+        return this.rowMap.computeIfAbsent(row, this.colMapSupplier);
+    }
+
+    private void removeIfEmpty(R row, Map<C, V> colMap) {
+        if (colMap.isEmpty()) {
+            this.rowMap.remove(row);
         }
     }
+    public interface Entry <R, C, V> {
+        R getRow();
+        C getCol();
+        V getValue();
+        V setValue(V value);
+    }
 
-    public void forEach(ForEachStoppableConsumer<R, C, V> consumer) {
-        for (Map.Entry<R, Map<C, V>> rowEntry : backingRowMap.entrySet()) {
-            R row = rowEntry.getKey();
-            for (Map.Entry<C, V> colEntry : rowEntry.getValue().entrySet()) {
-                C col = colEntry.getKey();
-                if (!consumer.accept(row, col, colEntry.getValue())) {
-                    return;
-                }
-            }
+    private class Node implements Entry <R, C, V> {
+
+        private final Map.Entry<R, Map<C, V>> rowEntry;
+        private final Map.Entry<C, V> colEntry;
+
+        Node(Map.Entry<R, Map<C, V>> rowEntry, Map.Entry<C, V> entry) {
+            this.rowEntry = rowEntry;
+            this.colEntry = entry;
+        }
+
+        @Override
+        public final R getRow() {
+            return rowEntry.getKey();
+        }
+
+        @Override
+        public final C getCol() {
+            return colEntry.getKey();
+        }
+
+        @Override
+        public final V getValue() {
+            return colEntry.getValue();
+        }
+
+        @Override
+        public final V setValue(V value) {
+            return colEntry.setValue(value);
         }
     }
-
-    public void replaceAll(BiFunction biFunction) {
-
-    }
-
-    public boolean remove(Object o, Object o1) {
-        return false;
-    }
-
-    @Nullable
-    public Object replace(Object o, Object o2) {
-        return null;
-    }
-
-    public Object computeIfAbsent(Object o, Function function) {
-        return null;
-    }
-
-    public Object computeIfPresent(Object o, BiFunction biFunction) {
-        return null;
-    }
-
-    public Object compute(Object o, BiFunction biFunction) {
-        return null;
-    }
-
-    public Object merge(Object o, Object o2, BiFunction biFunction) {
-        return null;
-    }
-
-    public boolean replace(R r, Map<C, V> cvMap, Map<C, V> v1) {
-        return false;
-    }
-
-    public Map<C, V> getOrDefault(Object o, Map<C, V> cvMap) {
-        return null;
-    }
-
-    @Nullable
-    public Map<C, V> putIfAbsent(R r, Map<C, V> cvMap) {
-        return null;
-    }
-
-
 }
